6a8f2c5b5440338dd6969d8e25762352b11c47f0
RomanKis <roman.kis.y@gmail.com>
2018-10-15 11:15:26 +0300

    LC2-56: Изменить вывод сообщения об ошибке

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,14 @@
+/**
+* Copyright 2016 aheadWorks. All rights reserved.
+* See LICENSE.txt for license details.
+*/
+
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/payment/default': {
+                'GoMage_LightCheckout/js/view/payment/default-mixin': true
+            }
+        }
+    }
+};
