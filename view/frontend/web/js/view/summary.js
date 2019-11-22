e4b5ffcd88a7e9201c0f6764013f888b0d612368
Rts <rts@gomage.com>
2019-11-22 12:18:55 +0200

    LC2-116

--- view/frontend/web/js/view/summary.js
+++ view/frontend/web/js/view/summary.js
@@ -10,19 +10,35 @@ define(
     function ($, Component) {
         'use strict';
         return Component.extend({
+            optional: 'Optional',
             afterRenderSummary: function () {
                 var loaderElement = '#checkout-loader .loader',
                     summaryElement = '.glc-right-col';
-
-                var checkExist = setInterval(function() {
+                var self = this;
+                var checkExist = setInterval(function () {
                     if (!$(loaderElement).is(':visible')) {
                         $(summaryElement).mage('sticky', {
                             container: '#maincontent'
                         });
+                        self.specialChangesFields();
                         clearInterval(checkExist);
                     }
                 }, 500);
+            },
+            /**
+             * add custom thing to design
+             */
+            specialChangesFields: function () {
+                console.log(this.optional);
+                $('label.label:contains('+this.optional+')').html(
+                    $('label.label:contains('+this.optional+')').html()
+                        .replace(
+                            '(' + this.optional + ')',
+                            '<span class="optional">(' + this.optional + ')</span>'
+                        )
+                );
             }
         });
     }
 );
+
